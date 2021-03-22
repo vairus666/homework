c30afb41caa11c816622b9fd21d4b65fc1b1f787
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-22 14:51:17 +0300

    Update helping.py

--- helping.py
+++ helping.py
@@ -1,20 +1,3 @@
-
-
-def write_to_file(data, filename="./text.txt"):
-    with open(filename, 'wb') as crip:
-        crip.write(data)
-
-
-def read_from_file(filename="./text.txt"):
-    with open(filename, 'rb') as crip:
-        data = crip.read()
-        return data
-
-def read_to_file_key(filename="./key.txt"):
-    with open(filename, 'r') as key_pass:
-        data = key_pass.read()
-        return data
-    
 def isint(s):
     # Проверка на целочисленность
     try:
